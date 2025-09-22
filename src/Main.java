b0a89b8737537949c88d34366d4d5e391fe8ddb7
bruteforce774 <danielaw@getacademy.no>
2025-09-22 19:59:35 +0200

    set up simple textbook example

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 import javafx.application.Application;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 
 public class Main extends Application {
-    @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("Hello, JavaFX!");
-        Scene scene = new Scene(label, 300, 200);
+        FlowPane panel = new FlowPane();
+        panel.setHgap(10);
+        Label etikett = new Label("Brukernavn:");
+        TextField brukernavn = new TextField();
+        brukernavn.setPrefColumnCount(25);
+        panel.getChildren().add(etikett);
+        panel.getChildren().add(brukernavn);
+        Scene scene = new Scene(panel, 250, 50);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("JavaFX Demo");
+        primaryStage.setTitle("Innlogging");
         primaryStage.show();
     }
 
